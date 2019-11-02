bee490e6f9847aa40990f31421cca7f2c184b1d6
julian-0 <jm.ord98@gmail.com>
2019-11-02 17:07:27 -0300

    Cambio de estructura de vista

--- View.py
+++ View.py
@@ -3,29 +3,47 @@ import tkinter as tk
 from Metodos import *
 
 
-class AppFinter():
+class AppFinter(Tk):
+
+    def __init__(self):
+        Tk.__init__(self)
+        self.title("Finter")
+        self.resizable(1, 1)
+        self.config(bg="light green")
+
+        container = Frame()
+        container.pack(side="top", fill="both", expand=True)
+        container.config(bg="dodger blue")
+        container.config(width="650", height="350")
+        container.config(relief="groove", bd=35)
+
+        self.frames = {}
+
+        frame = PaginaInicial(container, self)
+
+        self.frames[PaginaInicial] = frame
+
+        frame.grid(row=0, column=0, sticky="nsew")
+
+        self.mostrarFrame(PaginaInicial)
+
+
+    def mostrarFrame(self, vista):
+        frame = self.frames[vista]
+        frame.tkraise()
+
+class PaginaInicial(tk.Frame):
     ordenadas = []
     imagenes = []
 
-    def __init__(self):
-        raiz = Tk()
-        self.raiz = raiz
-        raiz.title("TP matematica superior")
-        raiz.resizable(1, 1)
-        raiz.config(bg="light green")
+    def __init__(self, padre, controlador):
+        tk.Frame.__init__(self, padre)
 
-        miFrame = Frame()
-        miFrame.pack()
-        miFrame.config(bg="dodger blue")
-        miFrame.config(width="650", height="350")
-
-        miFrame.config(relief="groove", bd=35)
-
-        Label(miFrame, text="Bienvenido!", bg="dodger blue").grid(row=1, column=0)
+        Label(self, text="Bienvenido!", bg="dodger blue").grid(row=1, column=0)
 
         # Frame Container
-        frame = LabelFrame(miFrame, text='Ingrese puntos')
-        frame.grid(row=3, column=0, pady=5)
+        frame = LabelFrame(self, text='Ingrese puntos')
+        frame.grid(row=3, column=0)
 
         # Ordenada Input
         Label(frame, text='Ordenada: ').grid(row=4, column=0)
@@ -46,13 +64,13 @@ class AppFinter():
         self.mensaje.grid(row=8, column=0, columnspan=2)
 
         # Tabla
-        self.tabla = ttk.Treeview(miFrame, height=10, columns=2)
+        self.tabla = ttk.Treeview(self, height=10, columns=2)
         self.tabla.grid(row=9, column=0)
         self.tabla.heading('#0', text='Ordenada', anchor=tk.CENTER)
         self.tabla.heading('#1', text='Imagen', anchor=tk.CENTER)
 
         # Seleccion de metodo
-        frameMetodo = LabelFrame(miFrame, text='Seleccione un metodo')
+        frameMetodo = LabelFrame(self, text='Seleccione un metodo')
         frameMetodo.grid(row=10, column=0, pady=5)
         self.combo = ttk.Combobox(frameMetodo, state="readonly", width=24)
         self.combo.pack()
@@ -60,8 +78,13 @@ class AppFinter():
         self.combo.set("Lagrange")
 
         # Boton calcular
-        botonCalcular = Button(miFrame, text="Calcular", bg="firebrick3", activebackground="darkOrchid4")
+        botonCalcular = Button(self, text="Calcular", bg="firebrick3", activebackground="darkOrchid4", command=self.ventanaCalcular)
         botonCalcular.grid(row=11, column=0)
+
+    def ventanaCalcular(self):
+        vCalcular = Toplevel()
+        vCalcular.title("Polinomio interpolante")
+        self.raiz.withdraw()
 
     def validar(self):
         if not self.completos():
@@ -76,13 +99,13 @@ class AppFinter():
         return len(self.ordenada.get()) != 0 and len(self.imagen.get()) != 0
 
     def numericos(self):
-        return self.ordenada.get().isdigit() and self.imagen.get().isdigit()
+        return self.ordenada.get().replace('.', '', 1).isdigit() and self.imagen.get().replace('.', '', 1).isdigit()
 
     def agregarPunto(self):
         self.mensaje['text'] = ''
         if self.validar():
-            self.ordenadas.append(self.ordenada.get())
-            self.imagenes.append(self.imagen.get())
+            self.ordenadas.append(float(self.ordenada.get()))
+            self.imagenes.append(float(self.imagen.get()))
             self.tabla.insert('', 'end', text=self.ordenada.get(), values=self.imagen.get())
         self.limpiarInputs()
 
@@ -90,7 +113,6 @@ class AppFinter():
         self.ordenada.delete(0, 'end')
         self.imagen.delete(0, 'end')
 
-
 if __name__ == '__main__':
     app = AppFinter()
-    app.raiz.mainloop()
+    app.mainloop()
